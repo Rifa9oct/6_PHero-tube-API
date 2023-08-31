a5be2877362687ae0fdfcb1386e6203f20902090
Rifa9oct <kohinuraktherr@gmail.com>
2023-09-01 01:59:32 +0600

    done show catagory sec

--- js/pHero.js
+++ js/pHero.js
@@ -24,7 +24,34 @@ const loadData = async() => {
 const handleLoadCatagory = async (catagoryId) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catagoryId}`);
     const data = await res.json();
-    console.log(data);
+    const dataItems = data.data;
+
+    const showCatagory = document.getElementById('show-catagory');
+    showCatagory.innerText = '';
+    dataItems.forEach(item => {
+        //console.log(item);
+        const div = document.createElement('div');
+        div.innerHTML =`
+        <div class="card bg-base-100 w-[312px] ">
+            <figure>
+            <img src="${item.thumbnail}" class="rounded-xl w-full h-[200px]" />
+            </figure>
+            <div class="flex gap-3 justify-between mt-5">
+                <img src="${item.authors[0].profile_picture}" class="rounded-full h-10 w-10">
+                <div>
+                    <h1 class="font-bold">Building a Winning UX Strategy Using the Kano Model</h1>
+                <div class="flex gap-2 my-2">
+                    <p class="text-[#171717b3] text-sm">${item.authors[0].profile_name}</p>
+                    <img src="item.others.thumbnail">
+                </div>
+                <h3 class="text-[#171717b3] text-sm pb-6">${item.others.views}</h3>
+                </div>
+            </div>
+        </div>
+        `;
+        showCatagory.appendChild(div);
+    });
+    
     
 }
 
